4a58ea4ccaf470d14828add301e31f7b165dc00f
c4605 <bolasblack@gmail.com>
2017-01-25 23:26:13 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import presetES2015 from 'babel-preset-es2015'
+import transformClassProperties from 'babel-plugin-transform-class-properties'
+import transformDoExpressions from 'babel-plugin-transform-do-expressions'
+import transformFunctionBind from 'babel-plugin-transform-function-bind'
+import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread'
+
+const buildPreset = (context, opts = {}) => {
+  const preset = { ...presetES2015.buildPreset(context, opts.es2015) }
+  preset.plugins = preset.plugins.concat([
+    transformClassProperties,
+    transformDoExpressions,
+    transformFunctionBind,
+    transformObjectRestSpread,
+  ])
+  return preset
+}
+
+const defaultPreset = buildPreset({})
+
+Object.defineProperty(defaultPreset, "buildPreset", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: buildPreset,
+})
