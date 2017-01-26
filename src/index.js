ae93519d5cb40dc516174b765755f4961f9ebf08
c4605 <bolasblack@gmail.com>
2017-01-26 21:52:16 +0800

    remove babel-preset-es2015, restrain node version >=6.9.4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import presetES2015 from 'babel-preset-es2015'
+import transformES2015ModulesCommonJS from "babel-plugin-transform-es2015-modules-commonjs"
+import transformES2015ModulesUMD from "babel-plugin-transform-es2015-modules-umd"
 import transformClassProperties from 'babel-plugin-transform-class-properties'
 import transformDoExpressions from 'babel-plugin-transform-do-expressions'
 import transformFunctionBind from 'babel-plugin-transform-function-bind'
 import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread'
 
 const buildPreset = (context, opts = {}) => {
-  const preset = { ...presetES2015.buildPreset(context, opts.es2015) }
-  preset.plugins = preset.plugins.concat([
-    transformClassProperties,
-    transformDoExpressions,
-    transformFunctionBind,
-    transformObjectRestSpread,
-  ])
+  let modules = 'commonjs'
+
+  if (opts.modules != null) modules = opts.modules
+
+  const preset = {
+    plugins: [
+      [transformClassProperties, opts.transformClassProperties],
+      [transformDoExpressions, opts.transformDoExpressions],
+      [transformFunctionBind, opts.transformFunctionBind],
+      [transformObjectRestSpread, opts.transformObjectRestSpread],
+      modules === "commonjs" && [transformES2015ModulesCommonJS, opts.transformES2015ModulesCommonJS],
+      modules === "umd" && [transformES2015ModulesUMD, opts.transformES2015ModulesUMD],
+    ].filter(Boolean)
+  }
   return preset
 }
 
